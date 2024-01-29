55d3d81fe215ed36d998e88b660ec114f6c39be1
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-01-29 02:26:22 +0000

    Jan 28, 2024, 6:26 PM

--- Page.h
+++ Page.h
@@ -13,5 +13,15 @@ private:
   string url;
   int visits;
   Page* links[3];
+
+public:
+  Page();
+  Page(string urlValue);
+  string getURL();
+  int getNumLinks();
+  Page* addLink(Page* other);
+  string getRandomLink();
+  void visit();
+  int getNumVistits();
 };
 #endif // PAGE_H
