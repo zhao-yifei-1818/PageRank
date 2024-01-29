253e943994a67b63e972d695ad1f058e744856fc
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-01-29 03:18:19 +0000

    Jan 28, 2024, 7:18 PM

--- Page.cpp
+++ Page.cpp
@@ -1,1 +1,56 @@
-// page.cpp
+
+/**
+ * @brief Assignment 3 PAGE Rank
+ * @name YOUR NAME
+ */
+#include "Page.h"
+
+Page::Page()
+{
+  url = "";
+  visits = 0;
+  for (int i; i < 3; i++) {
+    links[i] = nullptr;
+  };
+  usedLinks = 0;
+}
+// Should set url to "", both ints to 0 and all links to nullptr
+
+Page::Page(string urlValue)
+{
+  url = urlValue;
+}
+// Should set url to indicated value, everything else as above
+
+string Page::getURL()
+{
+  return url;
+}
+// Return the Page's URL
+
+int Page::getNumLinks()
+{
+  return usedLinks;
+}
+// Should return the number of usedLinks
+
+Page* Page::addLink(Page* other)
+{
+  links[usedLinks] =
+      other;      // store this pointer other to next available location,
+  usedLinks += 1; // increase usedlink.
+}
+// Should store the given pointer into the links array at the next available
+// location and increment the number of usedLinks
+
+string Page::getRandomLink()
+{
+  // major steps
+}
+// Should select a random address from the links the page has and return it.
+
+void visit();
+// Should add one to the number of visits this page has seen
+
+int getNumVistits();
+// Returns the number of visits for this page
