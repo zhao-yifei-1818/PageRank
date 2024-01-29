98eb2e721518645247659aff4630f8ce156b3a28
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-01-29 02:27:17 +0000

    Jan 28, 2024, 6:27 PM

--- Page.h
+++ Page.h
@@ -16,10 +16,20 @@ private:
 
 public:
   Page();
+  // Should set url to "", both ints to 0 and all links to nullptr
+
   Page(string urlValue);
+  // Should set url to indicated value, everything else as above
+
   string getURL();
+  // Return the Page's URL
+
   int getNumLinks();
+  // Should return the number of usedLinks
+
   Page* addLink(Page* other);
+  // Should store the given pointer into the links array at the next available
+  // location and increment the number of usedLinks
   string getRandomLink();
   void visit();
   int getNumVistits();
