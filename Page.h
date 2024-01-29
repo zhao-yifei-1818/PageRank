96ab778889f10237d38817b58cf97a9d90fa6c73
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-01-29 01:47:06 +0000

    Jan 28, 2024, 5:47 PM

--- Page.h
+++ Page.h
@@ -1,1 +1,17 @@
-// page/h
+#ifndef PAGE_H
+#define PAGE_H
+
+#include <string>
+
+using namespace std;
+
+/**
+ * @brief 111
+ */
+Class Page
+{
+private:
+  string url;
+  int visits;
+  Page* links[3];
+};
