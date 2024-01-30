aac4637862fb337fec02aa323a2844796d6ba40a
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-01-30 08:06:23 +0000

    Jan 30, 2024, 12:06 AM

--- main.cpp
+++ main.cpp
@@ -9,4 +9,19 @@ int main()
   Page page2;
   Page page3;
   Page page4;
+
+  Page* p1 = &page1;
+  Page* p2 = &page2;
+  Page* p3 = &page3;
+  Page* p4 = &page4;
+
+  page1.addLink(&page2);
+  page1.addLink(&page3);
+
+  page2.addLink(&page3);
+
+  page3.addLink(&page4);
+
+  page4.addLink(&page1);
+  page4.addLink(&page3);
 }
